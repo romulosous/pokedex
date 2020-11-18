d9d622bdeb3d7c50fbc4ac0da514d9639a95e2ab
Romulo Carvalho <romulocarvalho081@gmail.com>
2020-11-18 10:22:30 -0300

    instalando bibliotecas

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import axios from "axios"; //fazer requisições http dentro desse componente vue
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
