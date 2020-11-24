a00f0f27d5d40c9b7422b89cb7058a6b26b133d8
Romulo Carvalho <romulocarvalho081@gmail.com>
2020-11-24 17:48:02 -0300

    sistema de busca

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       <img src="./assets/images.png">
       <hr>
       <h4 class="is-size-4">Pokedex</h4>
-      <div v-for="(poke, index) in pokemons" :key="index">
+      <input type="text" class="input is-rounded" placeholder="Buscar pokemon pelo nome" v-model="busca">
+      <button class="button is-fullwidth is-success" id="buscaBtn" @click="buscar">Buscar</button>
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
         <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
       </div>
     </div>
@@ -21,14 +23,36 @@ export default {
   },
   data(){
     return {
-      pokemons: []
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
     }
   },
   created: function(){
     axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res => {
       console.log('pegou a lista de pokemons');
       this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
     })
+  },
+  methods: {
+    buscar(){
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ' ' ){
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+  },
+  computed: {
+    // resultadoBusca(){
+    //   if(this.busca == '' || this.busca == ' ' ){
+    //     return this.pokemons;
+    //   }else{
+    //     return this.pokemons.filter(pokemon => pokemon.name == this.busca);
+    //   }
+    // }
   }
 }
 </script>
@@ -41,5 +65,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#buscaBtn{
+  margin-top: 2%;
 }
 </style>
